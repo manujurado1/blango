330c5f94d9df253b75ed29bd474ac0be58512b9a
Manuel Trillo <m.jurado@jakincode.com>
2023-11-21 15:05:33 +0000

    Finish custom User model

--- blango/settings.py
+++ blango/settings.py
@@ -19,6 +19,7 @@ import dj_database_url
 class Dev(Configuration):
 	# Build paths inside the project like this: BASE_DIR / 'subdir'.
 	BASE_DIR = Path(__file__).resolve().parent.parent
+	AUTH_USER_MODEL = 'blango_auth.User'
 
 
 	# Quick-start development settings - unsuitable for production
@@ -49,6 +50,7 @@ class Dev(Configuration):
 	    'django.contrib.sessions',
 	    'django.contrib.messages',
 	    'django.contrib.staticfiles',
+      'blango_auth',
 	    'blog',
 	    'crispy_forms',
 	    'crispy_bootstrap5',
@@ -181,6 +183,7 @@ class Dev(Configuration):
         "level": "DEBUG",
     },
 	}
+
 
 class Prod(Dev):
     DEBUG = False
