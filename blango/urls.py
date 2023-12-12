e02e1bbcc375e94a7af26cd42263b2080c8f0fbf
Manuel Trillo <m.jurado@jakincode.com>
2023-12-12 11:45:04 +0000

    Finish JavaScript intro and fundamentals

--- blango/urls.py
+++ blango/urls.py
@@ -38,6 +38,7 @@ urlpatterns = [
     path("post/<slug>/", blog.views.post_detail, name="blog-post-detail"),
     path("ip/", blog.views.get_ip),
     path("api/v1/", include("blog.api.urls")),
+    path("post-table/", blog.views.post_table, name="blog-post-table"),
 ]
 
 if settings.DEBUG:
