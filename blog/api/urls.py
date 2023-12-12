111b90d5d219e2803f69e09e8ca61caf0274713a
Manuel Trillo <m.jurado@jakincode.com>
2023-12-12 09:57:27 +0000

    Finish simplejwt

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -5,6 +5,7 @@ from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 import os
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from blog.api.views import PostViewSet, UserDetail, TagViewSet
 
@@ -47,4 +48,6 @@ urlpatterns += [
         PostViewSet.as_view({"get": "list"}),
         name="posts-by-time",
     ),
+    path("jwt/", TokenObtainPairView.as_view(), name="jwt_obtain_pair"),
+    path("jwt/refresh/", TokenRefreshView.as_view(), name="jwt_refresh"),
 ]
